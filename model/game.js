b25dbac26158e7b211ae7eda0e06daa2690a841f
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-05-12 14:24:30 -0600

    add schema

---
+++ model/game.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+
+var Schema = mongoose.Schema
+var GameSchema = new Schema({
+  word: { type: String, required: true },
+  numGuess: { type: Number, required: true, default: 0 },
+  totalGuess: { type: Number, default: 2 },
+  victory: { type: Boolean, required: true, default: false  },
+  score: { type: Number, required: true, default: 0  },
+  letterGuess: { type: Array, required: true  },
+  startTime: { type: Number, required: true, default: Date.now()  },
+  endTime: { type: Number, required: true  }
+})
+
+var Game = mongoose.model('Game', GameSchema)
+
+module.exports = Game
