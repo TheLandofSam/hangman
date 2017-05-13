a63e8d52d936be1e99b33266ff78c97879d8125a
Darryl Kilzer <darrylkilzer@gmail.com>
2017-05-13 03:10:48 -0600

    sorry jason

--- public/controller.js
+++ public/controller.js
@@ -32,9 +32,10 @@ function Controller() {
     var charMap = getCharMap(data)
     var template = ''
     if (data.victory == true) {
-      template +=`
+      template += `
       <h1>win</h1>
       `
+      elem.innerHTML = template
     }
     for (var i = 0; i < charMap.length; i++) {
       var char = charMap[i];
@@ -45,48 +46,48 @@ function Controller() {
     elem.innerHTML = template
     button.innerHTML = buttonTemp
 
-      elem.innerHTML = template
-    }
-    if(data.maxGuesses == data.incorrect.length){
-      template +=`
+    
+  
+  if (data.maxGuesses == data.incorrect.length) {
+    template += `
       <h1>lose</h1>
       `
-      elem.innerHTML = template
-    }
-    else {
-      for (var i = 0; i < charMap.length; i++) {
-        var char = charMap[i];
-        buttonTemp += `
+    elem.innerHTML = template
+  }
+  else {
+    for (var i = 0; i < charMap.length; i++) {
+      var char = charMap[i];
+      buttonTemp += `
         <button onclick='app.controllers.ctrl.guess(event)' value='${charMap[i]}'>${charMap[i]}</button>
         `
-      }
-      template +=`
+    }
+    template += `
       <h3>${data.incorrect}</h3>
       <h3>${wordTemplate}</h3>
       `
-      elem.innerHTML = template
-      button.innerHTML = buttonTemp
-    }
-
+    elem.innerHTML = template
+    button.innerHTML = buttonTemp
   }
 
-  function fail(error) {
-    console.error("Something broke", error)
-  }
+}
 
-  function getCharMap(data) {
-    var usedLet = data.incorrect.concat([...data.correct])
-    var charMap = 'abcdefghijklmnopqrstubwxyz'.split('')
-    for (var i = 0; i < charMap.length; i++) {
-      var char = charMap[i]
-      for (var j = 0; j < usedLet.length; j++) {
-        var letter = usedLet[j];
-        if (char == letter) {
-          charMap.splice(i, 1)
-          i--
-        }
+function fail(error) {
+  console.error("Something broke", error)
+}
+
+function getCharMap(data) {
+  var usedLet = data.incorrect.concat([...data.correct])
+  var charMap = 'abcdefghijklmnopqrstubwxyz'.split('')
+  for (var i = 0; i < charMap.length; i++) {
+    var char = charMap[i]
+    for (var j = 0; j < usedLet.length; j++) {
+      var letter = usedLet[j];
+      if (char == letter) {
+        charMap.splice(i, 1)
+        i--
       }
     }
-    return charMap
   }
+  return charMap
+}
 }
