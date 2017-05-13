3d39be1ead36d392385bea30a73373a71d2f6d61
Darryl Kilzer <darrylkilzer@gmail.com>
2017-05-13 00:30:49 -0600

    button template

--- public/controller.js
+++ public/controller.js
@@ -8,22 +8,47 @@ function Controller() {
 
   this.guess = function (event) {
     event.preventDefault()
-    service.guess(event.target.guess.value, draw, fail)
+    service.guess(event.target.value, draw, fail)
   }
 
 
   function draw(data) {
     var elem = document.getElementById('display')
+    var button = document.getElementById('button')
+    var buttonTemp = ''
     var wordTemplate = data.word.join(' ')
+    var charMap = getCharMap(data)
     var template = `
       <h3>${data.incorrect}</h3>
       <h3>${wordTemplate}</h3>
       `
-
+      for (var i = 0; i < charMap.length; i++) {
+        var char = charMap[i];
+        buttonTemp += `
+        <button onclick='app.controllers.ctrl.guess(event)' value='${charMap[i]}'>${charMap[i]}</button>
+        
+        `
+      }
     elem.innerHTML = template
+    button.innerHTML = buttonTemp
   }
 
   function fail(error) {
     console.error("Something broke", error)
+  }
+  function getCharMap(data){
+    var usedLet = data.incorrect.concat([...data.correct])
+    var charMap = 'abcdefghijklmnopqrstubwxyz'.split('')
+        for (var i = 0; i < charMap.length; i++) {
+      var char = charMap[i]
+      for (var j = 0; j < usedLet.length; j++) {
+        var letter = usedLet[j];
+        if(char == letter){
+          charMap.splice(i, 1)
+          i--
+        }
+      }
+    }
+    return charMap
   }
 }
