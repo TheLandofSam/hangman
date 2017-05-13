74d8da946e8b8a1b08ae19cba2cd7d5abd398a00
jasonlietnguyen@gmail.com <jason123nguyen@gmail.com>
2017-05-12 21:31:06 -0600

    testing 123

--- public/controller.js
+++ public/controller.js
@@ -3,7 +3,18 @@ function Controller(){
   var service = new Service()
 
   this.startGame = function (){
-    service.startGame()
+    service.startGame(draw,fail)
   }
 
+  this.guess = function (event) {
+    event.preventDefault()
+    service.guess(event.target.guess.value, draw, fail)
+  }
+
+
+  function draw(){}
+
+  function fail(error){
+    console.error("Something broke", error)
+  }
 }
--- public/service.js
+++ public/service.js
@@ -1,10 +1,23 @@
 function Service() {
-  var url = 'http://localhost:4545/game'
+  var url = 'http://localhost:4545/game/start-game'
+  var url2 = 'http://localhost:4545/game/guess'
+  var currentGameId = ''
 
-    this.startGame = function () {
+  
+    this.startGame = function (draw, fail) {
       $.post(url).then(function (data) {
         console.log(data)
-      })
+        draw(data)
+        currentGameId = data._id
+      }).catch(fail)
     }
+
+    this.guess = function (val, draw, fail) {
+      $.post(url2, {letter: val, _id: currentGameId}).then(function (event) {
+        draw(event)
+      }).catch(fail)
+    }
+
+
 
 }
